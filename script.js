bde0bbcd6e4b6f89f7d761df98a874a0c4a36e1f
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-09-14 01:36:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log("Let's write JS");
+
+function checkTheParameters() {
+    const a = parseFloat(document.querySelector("#a").value);
+    const h = parseFloat(document.querySelector("#h").value);
+    const b = parseFloat(document.querySelector("#b").value);
+    const g = parseFloat(document.querySelector("#g").value);
+    const f = parseFloat(document.querySelector("#f").value);
+    const c = parseFloat(document.querySelector("#c").value);
+
+    let mat = [
+        [a,h,g],
+        [h,b,f],
+        [g,f,c]
+    ]
+
+    let det = a * (b * c - f * f) - h * (h * c - g * f) + g * (h * f - b * g);    
+    
+    let D = a*b - h**2
+
+    let main2Div = document.querySelector(".main2");
+    
+    
+    if (isNaN(a) || isNaN(h) || isNaN(b) || isNaN(g) || isNaN(f) || isNaN(c)) {
+        main2Div.innerHTML = `<section style="color: red">Please Enter complete data</section>`
+        main2Div.style.display="block";
+    } else if (det === 0) {
+        main2Div.innerHTML = `
+        <div class="delta">Delta = ${a}(${b} * ${c} - ${f} * ${f}) - ${h}(${h} * ${c} - ${g} * ${f}) + ${g}(${h} * ${f} - ${b} * ${g})</div>
+        <div class="deltaa">Delta = ${det}</div>
+        <div class="D">D = ${D}</div>
+        <div class="type">The given Equation represents a pair of Straight Line</div>`
+        main2Div.style.display="block";
+    } else if (det !== 0 && D>0) {
+        main2Div.innerHTML = `
+        <div class="delta">Delta = ${a}(${b} * ${c} - ${f} * ${f}) - ${h}(${h} * ${c} - ${g} * ${f}) + ${g}(${h} * ${f} - ${b} * ${g})</div>
+        <div class="deltaa">Delta = ${det}</div>
+        <div class="D">D = ${D}</div>
+        <div class="type">The given Equation represents a Ellipse</div>`
+        main2Div.style.display="block";
+    } else if (det !== 0 && a+b!==0 && D<0) {
+        main2Div.innerHTML = `
+        <div class="delta">Delta = ${a}(${b} * ${c} - ${f} * ${f}) - ${h}(${h} * ${c} - ${g} * ${f}) + ${g}(${h} * ${f} - ${b} * ${g})</div>
+        <div class="deltaa">Delta = ${det}</div>
+        <div class="D">D = ${D}</div>
+        <div class="type">The given Equation represents a Hyperbola</div>`
+        main2Div.style.display="block";
+    } else if (det !== 0 && a+b===0 && D<0) {
+        main2Div.innerHTML = `
+        <div class="delta">Delta = ${a}(${b} * ${c} - ${f} * ${f}) - ${h}(${h} * ${c} - ${g} * ${f}) + ${g}(${h} * ${f} - ${b} * ${g})</div>
+        <div class="deltaa">Delta = ${det}</div>
+        <div class="D">D = ${D}</div>
+        <div class="type">The given Equation represents a Rectangular Hyperbola</div>`
+        main2Div.style.display="block";
+    } else if (det !== 0 && D===0) {
+        main2Div.innerHTML = `
+        <div class="delta">Delta = ${a}(${b} * ${c} - ${f} * ${f}) - ${h}(${h} * ${c} - ${g} * ${f}) + ${g}(${h} * ${f} - ${b} * ${g})</div>
+        <div class="deltaa">Delta = ${det}</div>
+        <div class="D">D = ${D}</div>
+        <div class="type">The given Equation represents a Parabola</div>`
+        main2Div.style.display="block";
+    } else if (det !== 0 && a===b && h===0) {
+        main2Div.innerHTML = `
+        <div class="delta">Delta = ${a}(${b} * ${c} - ${f} * ${f}) - ${h}(${h} * ${c} - ${g} * ${f}) + ${g}(${h} * ${f} - ${b} * ${g})</div>
+        <div class="deltaa">Delta = ${det}</div>
+        <div class="D">D = ${D}</div>
+        <div class="type">The given Equation represents a Circle</div>`
+        main2Div.style.display="block";
+    }
+}
